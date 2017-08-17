2e43ebfed2f193844d3bf62fdcc0322985e705ea
endrx <endymionr@gmail.com>
2017-08-17 15:23:44 +0300

    init

---
+++ eventsDiagram.js
@@ -0,0 +1,320 @@
+/*
+eventsDiagram
+Copyright (c) 2017 endrx - endymionr@gmail.com
+MIT License Applies
+*/
+
+; (function ($) {
+    "use strict";
+	
+    let EventsDiagram = function (element, options) {
+
+        let control = this;
+        control.options = $.extend({}, $.fn.eventsDiagram.Defaults, options);
+        control.$element = $(element);
+
+        init();
+
+        function init() {
+
+            //control.$element.css("box-sizing", "border-box").css("border", "1px solid magenta");
+
+            control.$mainContainer = $("<div />").addClass("ed-container");
+            control.$cornerHeader = $("<div />").addClass("ed-corner-header");
+            control.$cornerHeaderInside = $("<div />");
+            control.$cornerHeader.append(control.$cornerHeaderInside);
+            control.$leftHeader = $("<div />").addClass("ed-left-header");
+            control.$topHeader = $("<div />").addClass("ed-top-header");
+            control.$grid = $("<div />").addClass("ed-grid");
+
+            control.$mainContainer
+                .append(control.$cornerHeader)
+                .append(control.$leftHeader)
+                .append(control.$topHeader)
+                .append(control.$grid);
+
+            control.$element
+                .append(control.$mainContainer);
+
+            control.$mainContainer.on("scroll", function (e) {
+                let topOffset = e.currentTarget.scrollTop;
+                let leftOffset = e.currentTarget.scrollLeft;
+                control.$cornerHeader.css("left", leftOffset + "px");
+                control.$cornerHeader.css("top", topOffset + "px");
+                control.$topHeader.css("top", topOffset + "px");
+                control.$leftHeader.css("left", leftOffset + "px");
+            });
+
+            setLeftHeaderWidth(control.options.leftHeadersWidth);
+            control.$cornerHeaderInside.html(control.options.cornerHtml);
+            buildLeftHeaders();
+            buildTopHeaders();
+
+            buildGrid();
+            buildBarRows();
+
+            processHeights();
+        }
+
+        function setLeftHeaderWidth(newWidth) {
+            control.$cornerHeaderInside.css("width", newWidth);
+            control.$leftHeader.css("width", newWidth);
+            control.$topHeader.css("margin-left", newWidth);
+            control.$grid.css("margin-left", newWidth);
+        }
+
+        function buildCalendar() {
+            let timeRange = findTimeRanges(control.options.events);
+            let timeRangeDates = { beginDate: getTimeDay(timeRange.beginTime), endDate: getTimeDay(timeRange.endTime) };
+
+            control.gridBeginTime = new Date(timeRange.beginTime.getFullYear(), timeRange.beginTime.getMonth(), timeRange.beginTime.getDate());
+            control.gridEndTime = new Date(timeRange.endTime.getFullYear(), timeRange.endTime.getMonth(), timeRange.endTime.getDate());
+
+            control.months = {};
+            control.days = [];
+
+            enumerateDays(control.gridBeginTime, control.gridEndTime, function (day) {
+                let month = day.getFullYear() + "/" + (day.getMonth() + 1);
+                control.months[month] = 1 + (control.months[month] || 0);
+                control.days.push(day.getDate());
+            });
+        }
+
+        function buildTopHeaders() {
+
+            control.$topHeader.empty();
+
+            buildCalendar();
+
+            let $monthsRow = $("<div />");
+            let $daysRow = $("<div />")
+
+            for (let monthName in control.months) {
+                var month = control.months[monthName];
+                let $month = $("<div />")
+                    .addClass("ed-calend-month")
+                    .css("width", control.options.columnWidth * month + "px")
+                    .text(monthName);
+                $monthsRow.append($month);
+            }
+            control.$topHeader.append($monthsRow);
+
+            for (let dayIndex in control.days) {
+                var day = control.days[dayIndex];
+                let $day = $("<div />")
+                    .addClass("ed-calend-day")
+                    .css("width", control.options.columnWidth + "px")
+                    .text(day);
+                $daysRow.append($day);
+            }
+            control.$topHeader.append($daysRow);
+        }
+
+        function buildLeftHeaders() {
+
+            control.rowHeights = [];
+
+            function buildLeftHeadersRec(events, indent) {
+                $.each(events, function (i, event) {
+
+                    let $leftHeaderRow = $("<div />")
+                                    .addClass("ed-left-header-item")
+                                    .css("padding-left", indent + "px")
+                                    .html(event.name);
+
+                    control.$leftHeader.append($leftHeaderRow);
+
+                    if (typeof event.events !== "undefined" && event.events != null) {
+                        buildLeftHeadersRec(event.events, indent + options.leftHeaderIndentStep);
+                    }
+
+                });
+            }
+            buildLeftHeadersRec(control.options.events, 5);
+
+        }
+
+        function buildBarRows() {
+
+            let $barRows = $("<div />").addClass("ed-bar-container");
+
+            function buildBarRowsRec(events) {
+                $.each(events, function (i, event) {
+
+                    let $barRow = $("<div />")
+                                .addClass("ed-bar-row")
+                                .css("width", control.options.columnWidth * control.days);
+
+                    let episodes = getEventEpisodes(event);
+
+                    $.each(episodes, function (ii, episode) {
+                        let $bar = $("<div />")
+                                    .addClass("ed-bar")
+                                    .css("left", getPixelSize({ beginTime: control.gridBeginTime, endTime: episode.beginTime }))
+                                    .css("width", getPixelSize({ beginTime: episode.beginTime, endTime: episode.endTime }))
+                                    .css("background-color", episode.color);
+                        $barRow.append($bar);
+                    });
+
+                    $barRows.append($barRow);
+
+                    if (typeof event.events !== "undefined" && event.events != null) {
+                        buildBarRowsRec(event.events);
+                    }
+                });
+            }
+
+            buildBarRowsRec(control.options.events);
+
+            control.$grid.append($barRows);
+        }
+
+        function buildGrid() {
+
+            let $gridCells = $("<div />");
+
+            function buildGridRec(events) {
+
+                $.each(events, function (i, event) {
+
+                    let $cellsRow = $("<div />").addClass("ed-cell-row");
+
+                    $.each(control.days, function (i, day) {
+
+                        let $cell = $("<div />")
+                            .addClass("ed-cell")
+                            .css("width", control.options.columnWidth + "px");
+                        $cellsRow.append($cell);
+                });
+
+                    $gridCells.append($cellsRow);
+
+                    if (typeof event.events !== "undefined" && event.events != null) {
+                        buildGridRec(event.events);
+                }
+            });
+
+            }
+
+            buildGridRec(control.options.events);
+
+            control.$grid.append($gridCells);
+        }
+
+        function processHeights() {
+            setTimeout(function () {
+
+                //let heights = $.map(control.$leftHeader.find(".ed-left-header-item"), function (el) {
+                //    return el.offsetheight;
+                //});
+
+                let heights = [];
+                let totalHeight = 0;
+                let lhs = control.$leftHeader.find(".ed-left-header-item");
+                for (let i = 0; i < lhs.length; i++) {
+                    let height = i < lhs.length - 1 ? lhs[i + 1].offsetTop - lhs[i].offsetTop : lhs[i].offsetHeight;
+                    heights.push(height);  
+                    totalHeight += height;
+                }
+
+                $.each(control.$grid.find(".ed-cell-row"), function (i, el) {
+                    $(el).height(heights[i]);
+                });
+                //control.$grid.find(".ed-bar-container")
+                //        .css("top", -totalHeight + "px");
+                $.each(control.$grid.find(".ed-bar-row"), function (i, el) {
+                    let height = heights[i];
+                    $(el)
+                        .height(height)
+                        .find(".ed-bar").height(height > 20 ? 14 : height - 6);
+                });
+
+            }, 1);
+        }
+
+
+        function findTimeRanges(events) {
+
+            let maxTime = new Date(-8640000000000000);
+            let minTime = new Date(8640000000000000);
+
+            function findTimeRangesInt(events) {
+
+                $.each(events, function (i, event) {
+                    let episodes = getEventEpisodes(event);
+                    $.each(episodes, function (ii, episode) {
+                        if (minTime > episode.beginTime) minTime = episode.beginTime;
+                        if (maxTime < episode.beginTime) maxTime = episode.beginTime;
+                        if (minTime > episode.endTime) minTime = episode.endTime;
+                        if (maxTime < episode.endTime) maxTime = episode.endTime;
+                    });
+
+                    if (event.events != null)
+                        findTimeRangesInt(event.events);
+                });
+            }
+
+            findTimeRangesInt(events);
+
+            return {
+                beginTime: minTime, endTime: maxTime
+            };
+        }
+        function getTimeDay(time) {
+            var day = new Date(time.getFullYear(), time.getMonth(), time.getDate());
+            return day;
+        }
+
+        function getPixelSize(timeRange) {
+            let oneDay = 24 * 60 * 60 * 1000;
+            let res = Math.round(Math.abs((timeRange.endTime.getTime() -timeRange.beginTime.getTime()) * control.options.columnWidth / oneDay));
+            return res;
+        }
+
+        function enumerateDays(beginTime, endTime, cb) {
+            let beginDay = getTimeDay(beginTime);
+            let endDay = getTimeDay(endTime);
+            for (let day = beginDay; day <= endDay; day.setDate(day.getDate() + 1)) {
+                cb(day);
+            }
+        }
+
+        function getEventEpisodes(event) {
+            let episodes = typeof event.episodes !== "undefined" ? event.episodes : [{
+                beginTime: event.beginTime,
+                endTime: event.endTime,
+                color: event.color
+            }];
+            return episodes;
+        }
+
+    }
+
+    EventsDiagram.Version = "0.4.1";
+
+    EventsDiagram.Defaults = {
+        events: [],
+        leftHeadersWidth: "200px",
+        leftHeaderIndentStep: 20,
+        columnWidth: 20,
+        cornerHtml: ""
+    };
+
+
+    function Plugin(option) {
+        return this.each(function () {
+            var $this = $(this);
+            var data = $this.data("eventsDiagram");
+
+            var options = $.extend({}, EventsDiagram.Defaults, $this.data(), typeof option == 'object' && option);
+
+            if (!data) $this.data('eventsDiagram', (data = new EventsDiagram(this, options)))
+            if (typeof option == 'string') data[option]()
+        })
+    }
+
+    $.fn.eventsDiagram = Plugin;
+    $.fn.eventsDiagram.Defaults = EventsDiagram.Defaults;
+    $.fn.eventsDiagram.Constructor = EventsDiagram;
+
+})(jQuery);
